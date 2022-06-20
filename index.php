5cfa4163a5e10e4666a14efb0dd309409974429b
Apollo11-hub <Antonio-Pollo@outlook.it>
2022-06-21 00:17:17 +0200

    inserimento user

--- index.php
+++ index.php
@@ -1,9 +1,10 @@
 <?php 
 
 require_once __DIR__ . "/classes/Products.php";
+require_once __DIR__ . "/classes/User.php";
 require_once __DIR__ . "/classes/AnimalTypeProduct.php";
 
-
+//PRODUCTS
 $cuccia_type1 = new AnimalTypeProduct('Cuccia' , 29.99 , true,);
 $cuccia_type1->setQuantity(6);
 $cuccia_type1->setWeight(7);
@@ -18,13 +19,18 @@ $food_type1->setFlavor("Agnello e Verdure");
 $food_type1->setSpecificName("Purina Dog Crunchy");
 $food_type1->setAnimalSpecies("Cane");
 
-
-
-
-
-
 var_dump($cuccia_type1);
 var_dump($food_type1);
+
+
+
+
+//USER
+$luigi_rossi = new User("Luigi","Rossi","luigi.rossi@mail.it",true);
+
+var_dump($luigi_rossi);
+
+
 ?>
 
 
